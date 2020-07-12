17c2b8c63f1f06f9644f9792ade312076cb06b87
Himanshu Sharma <himanshu.fourtek@gmail.com>
2020-07-12 13:32:11 +0530

    Update Primenumber_generator.py

--- Primenumber_generator.py
+++ Primenumber_generator.py
@@ -1,19 +1,3 @@
-"""class PrimeGenerator():
-    def __init__(self,stop):
-        self.start = 2
-        self.stop = stop
-
-    def __next__(self):
-        if self.start < self.stop:
-            self.start +=1
-            for x in range(2, self.start):
-                if self.start % x == 0 :
-                    self.start += 1
-            return self.start
-        else:
-            raise StopIteration()"""
-
-
 class PrimeGenerator:
     def __init__(self, stop):
         self.stop = stop
